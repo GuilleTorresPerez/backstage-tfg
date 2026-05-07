a180dcb5fe4deb0d6ad16dafb7d91f752b1b04e3
GuilleTorresPerez <guilletorresperez@gmail.com>
2026-05-07 18:18:11 +0200

    add OIDC authentication provider with Keycloak integration

--- packages/app/src/apis.ts
+++ packages/app/src/apis.ts
@@ -5,15 +5,50 @@ import {
 } from '@backstage/integration-react';
 import {
   AnyApiFactory,
+  BackstageIdentityApi,
   configApiRef,
   createApiFactory,
+  createApiRef,
+  discoveryApiRef,
+  OAuthApi,
+  oauthRequestApiRef,
+  ProfileInfoApi,
+  SessionApi,
 } from '@backstage/core-plugin-api';
+import { OAuth2 } from '@backstage/core-app-api';
+
+export const oidcAuthApiRef = createApiRef<
+  OAuthApi & ProfileInfoApi & BackstageIdentityApi & SessionApi
+>({
+  id: 'auth.oidc',
+});
 
 export const apis: AnyApiFactory[] = [
   createApiFactory({
     api: scmIntegrationsApiRef,
     deps: { configApi: configApiRef },
     factory: ({ configApi }) => ScmIntegrationsApi.fromConfig(configApi),
+  }),
+  createApiFactory({
+    api: oidcAuthApiRef,
+    deps: {
+      discoveryApi: discoveryApiRef,
+      oauthRequestApi: oauthRequestApiRef,
+      configApi: configApiRef,
+    },
+    factory: ({ discoveryApi, oauthRequestApi, configApi }) =>
+      OAuth2.create({
+        configApi,
+        discoveryApi,
+        oauthRequestApi,
+        provider: {
+          id: 'oidc',
+          title: 'Keycloak (Aragón IdP)',
+          icon: () => null,
+        },
+        environment: configApi.getOptionalString('auth.environment'),
+        defaultScopes: ['openid', 'profile', 'email'],
+      }),
   }),
   ScmAuth.createDefaultApiFactory(),
 ];
